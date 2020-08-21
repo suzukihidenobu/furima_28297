a1e749b86a678a91931e0600aa0d2f1cfe2c9e9f
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-08-21 19:59:40 +0900

    Application Controllerを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :first_name, :last_name,
+      :first_name_kana, :last_name_kana, :birthday, :email, :encrypted_password])
+  end
 
 
   private
@@ -10,3 +17,12 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
+t.string :nick_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :first_name_kana, null: false
+      t.string :last_name_kana, null: false
+      t.date :birthday, null: false
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: "
