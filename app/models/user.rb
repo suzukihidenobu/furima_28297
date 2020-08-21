48f48d7eb334583a5589370b9bf8710eb951bcd4
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-08-21 21:10:21 +0900

    Userモデルを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nick_name, :first_name, :last_name, 
+            :first_name_kana, :last_name_kana, 
+            :birthday, :email, :encrypted_password ,presence: true
 end
 
 
