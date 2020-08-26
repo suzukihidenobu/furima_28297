8beadb384e813a8cd0bb0e0f1c568b3858b461ec
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-08-26 21:11:54 +0900

    配送について記述

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,15 +1,12 @@
-class Genre < ActiveHash::Base
+class Condition < ActiveHash::Base
   self.data = [
               {id: 0, name: '---'},
-              {id: 1, name: 'レディース'},
-              {id: 2, name: 'メンズ'}, 
-              {id: 3, name: 'ベビー・キッズ'},
-              {id: 4, name: 'インテリア・住まい・小物'}, 
-              {id: 5, name: '本・音楽・ゲーム'},
-              {id: 6, name: 'おもちゃ・ホビー・グッズ'},
-              {id: 7, name: '家電・スマホ・カメラ'},
-              {id: 8, name: 'スポーツ・レジャー'}, 
-              {id: 9, name: 'ハンドメイド'}, 
-              {id: 10, name: 'その他'}, 
+              {id: 1, name: '新品、未使用'},
+              {id: 2, name: '未使用に近い'}, 
+              {id: 3, name: '目立った傷や汚れなし'},
+              {id: 4, name: 'やや傷や汚れあり'}, 
+              {id: 5, name: '傷や汚れあり'},
+              {id: 6, name: '全体的に状態が悪い'},
               ]
 end
+
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,5 @@
 class Genre < ActiveHash::Base
+  
   self.data = [
               {id: 0, name: '---'},
               {id: 1, name: 'レディース'},
