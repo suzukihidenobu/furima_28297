2aaf74bf98a3abff0917e614faeb0486f3bbf98a
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-13 17:26:36 +0900

    Revert "Revert "Revert "ユーザー管理機能の実装2"""
    This reverts commit 6ea590bae92e8e9721848fc82b8edb7bf72ec532.

--- app/controllers/articles_controller.rb
+++
@@ -1,8 +0,0 @@
-class ArticlesController < ApplicationController
-  def index
-    @articles = Article.order('created_at DESC') # 記事一覧が新規投稿順に並ぶように記述します。
-  end
-
-  def new
-  end
-end
--- app/models/purchase.rb
+++
@@ -1,5 +0,0 @@
-class Purchase < ApplicationRecord
-  has_one :address
-  belongs_to :user # cardモデル（子モデル）user（親モデル）が削除された時にcard（子モデル）も削除される、
-  belongs_to :item
-end
