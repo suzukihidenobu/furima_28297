54c65d61ebf9076dde23f224c5fb6d82b54d2782
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-14 21:28:17 +0900

    ホーム画面に遷移

---
+++ app/javascript/price.js
@@ -0,0 +1,18 @@
+
+function profit(){
+  const price = document.getElementById("item-price");
+
+  price.addEventListener("change", function(){
+    let profit = price.value;
+    const tax = document.getElementById("add-tax-price");
+    let fee = profit * 0.1
+    tax.textContent = fee
+
+    const tprofit = document.getElementById("profit");
+    tprofit.textContent = profit - fee
+    
+  });
+};
+window.addEventListener("load", profit);
+
+
