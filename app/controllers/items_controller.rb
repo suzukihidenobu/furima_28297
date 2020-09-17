7840756ff9e29541bcea7918bb4f6e84b643b843
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-17 11:09:03 +0900

    不要な部分を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,15 @@
-class ItemsController < ApplicationController  # アッパーキャメルケース先頭から単語の区切りを大文字で表す
+class ItemsController < ApplicationController 
   before_action :set_item, only: [:edit, :show, :update]
   before_action :authenticate_user!, only: [:new, :edit]
-
-  # before_action :move_to_index, except: [:index, :show]
-
+  
   def index
     @items = Item.all
-    # allメソッドを使用して、timssテーブルすべてのレコードをインスタンス変数に代入し、ビューに受け渡します。
-    @items = Item.order('created_at DESC') # 記事一覧が新規投稿順に並ぶように記述します。
+  ビューに受け渡します。
+    @items = Item.order('created_at DESC') します。
   end
 
   def new
-    @item = Item.new # items_controller.rbにnewアクションを定義します。
+    @item = Item.new 
   end
 
   def create
@@ -19,11 +17,9 @@ class ItemsController < ApplicationController  # アッパーキャメルケー�
     if @item.valid?
       @item.save
       redirect_to root_path
-
     else
       render :new
     end
-    # Item.create(item_params) # createアクション内のcreateメソッドで実行します。
   end
 
   def destroy
@@ -31,10 +27,6 @@ class ItemsController < ApplicationController  # アッパーキャメルケー�
     item.destroy
     redirect_to root_path
   end
-
-  # def edit
-
-  # end
 
   def update
     if @item.update(item_params)
@@ -45,11 +37,9 @@ class ItemsController < ApplicationController  # アッパーキャメルケー�
   end
 
   def show
-    # @item = Item.find(params[:id])
   end
 
-  # item_paramsというストロングパラメーターを定義し、createメソッドの引数に使用して、itemssテーブルへ保存できるようにしました。
-  private
+ private
 
   def item_params
     params.require(:item).permit(:image, :name, :price, :item_info, :category_name_id, :condition_id,
