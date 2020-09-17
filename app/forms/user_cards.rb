e513ca2cd34538d18917811c3d9e6736126c7652
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-17 21:34:13 +0900

    purchase 修正

--- app/forms/user_cards.rb
+++ app/forms/user_cards.rb
@@ -1,7 +1,7 @@
 class UserCards
   include ActiveModel::Model
   attr_accessor :name, :name_reading, :nickname, :postal_code, :shipping_area,
-                :city, :phone, :building, :price, :shipping_area, :address_number, :commit, :item_id, :token, :user_id, :purchase_id
+                :city, :phone, :building, :price, :shipping_area, :address_number, :commit, :item_id, :token, :user_id, :purchases_id
 
   # 「住所」の郵便番号に関するバリデーション
   # validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -21,8 +21,8 @@ class UserCards
     # user = UserCards.create(name: name, name_reading: name_reading, nickname: nickname)
     # 住所の情報を保存
     # binding.pry
-    purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, address_number: address_number, phone: phone, city: city, building: building, shipping_area: shipping_area, purchase_id: purchase_id)
+    purchases = Purchase.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, address_number: address_number, phone: phone, city: city, building: building, shipping_area: shipping_area, purchases_id: purchases_id)
     # binding.pry
     # 寄付金の情報を保存
   end
