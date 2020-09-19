028c1351cd1a583a94e67cf53135bc3da27a3faa
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-19 21:01:37 +0900

    sould outの時に『購入画面に進む』が消えている

--- app/forms/user_cards.rb
+++ app/forms/user_cards.rb
@@ -12,7 +12,6 @@ class UserCards
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    binding.pry
     Address.create(postal_code: postal_code, address_number: address_number, phone: phone, city: city, building: building, shipping_area: shipping_area, purchase_id: purchase.id)
   end
 end
