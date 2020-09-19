851cdd89ed52db6ba479860a93d07a0f02421130
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-19 19:03:31 +0900

    Siquel Ploに購入した際の住所情報は保存できるようにした。

--- app/forms/user_cards.rb
+++ app/forms/user_cards.rb
@@ -6,13 +6,13 @@ class UserCards
   validates :shipping_area, numericality: { other_than: 0, message: "can't be blank" }
   validates :city, presence: true
   validates :address_number, presence: true
-  validates :building, presence: true
   validates :phone, presence: true
   validates :postal_code, presence: true
   validates :shipping_area, presence: true
 
   def save
-    purchases = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, address_number: address_number, phone: phone, city: city, building: building, shipping_area: shipping_area, purchases_id: purchases_id)
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    binding.pry
+    Address.create(postal_code: postal_code, address_number: address_number, phone: phone, city: city, building: building, shipping_area: shipping_area, purchase_id: purchase.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,15 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2020_09_04_003308) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -27,8 +39,8 @@ ActiveRecord::Schema.define(version: 2020_09_04_003308) do
     t.string "city", default: "", null: false
     t.string "address_number", default: "", null: false
     t.string "building", default: ""
-    t.integer "phone"
-    t.integer "purchases_id", null: false
+    t.string "phone", default: ""
+    t.integer "purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
