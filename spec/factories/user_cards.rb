d173985bca1b7abacd1b1409cd0d469e7e81baec
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-22 19:34:16 +0900

    余分な部分を削除

--- spec/factories/user_cards.rb
+++ spec/factories/user_cards.rb
@@ -8,35 +8,5 @@ FactoryBot.define do
     phone { '09012345678' }
   end
 end
-  # FactoryBot.define do
-  #     factory :user do
-  #       nickname              {Faker::Name.initials(number: 2)}
-  #       email                 {Faker::Internet.free_email}
-  #       password              {Faker::Internet.password(min_length: 6)}
-  #       password_confirmation {password}
-  #       first_name            { '鈴木' }
-  #       last_name             {  '一郎' }
-  #       first_name_kana       { 'スズキ' }
-  #       last_name_kana        { 'イチロ' }
-  #       birthday              { '1977-12-24' }
-  #     end
-  #   end
-  #     FactoryBot.define do
-  #     factory :item do
-  #       name { '商品名' }
-  #       item_info { '商品の説明(必須 1000文字まで)' }
-  #       category_name_id { 'メンズ' }
-  #       condition_id { '新品未使用' }
-  #       delivery_burden_id {  '着払い(購入者負担)' }
-  #       shipping_area_id { '東京都' }
-  #       shipping_days_id { '1~2日で発送' }
-  #       price {'80000'}
-  #       association :user 
-    
-  #       after(:build) do |item|
-  #         item.image.attach(io: File.open('public/images/IMG_7786.jpg'), filename: 'IMG_7786.jpg')
-  #       end
-  #     end
-  #   end
 
 
