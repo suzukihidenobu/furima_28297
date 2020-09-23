0f9fcfd9a3f544cc16ae7f349940c70b02c1e936
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-23 15:04:56 +0900

    削除修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name,
-                                                       :first_name_kana, :last_name_kana, :birthday, :email, :encrypted_password])
-
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
- end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name,:first_name_kana, :last_name_kana, :birthday, :email, :encrypted_password])
+  end
 
   private
 
@@ -16,5 +13,5 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'furima' && password == 'Suki5995'
     end
-    end
+  end
 end
