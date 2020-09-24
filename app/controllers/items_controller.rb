35a6710e4cdf517ab953fdad07380028d57abd86
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-24 19:45:56 +0900

    削除機能修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,13 +22,22 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id
-      @item.destroy
+    item = Item.find(params[:id])
+ 
+    if current_user.id == item.user_id
+      item.destroy
       redirect_to root_path
     else
       render 'show'
     end
   end
+
+
+
+
+
+
+  
 
   def edit
   end
