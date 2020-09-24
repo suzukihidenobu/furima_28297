6e66c10c4585f53971279423fb194a128f5d8007
suzukihidenobu <68547616+suzukihidenobu@users.noreply.github.com>
2020-09-24 12:12:13 +0900

    必須項目が一つでも欠けている場合は、出品ができないようにしました

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,17 +11,22 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_many :items
   has_one :item_order
+  
 
   with_options presence: true do
     validates :image
     validates :name
     validates :item_info
+    validates :price, format: { with: /\A[a-z0-9]+\z/i, message: 'は半角英数で入力してください。' }
+  end
+
+  with_options numericality: { other_than: 0, message: "を選択してください" } do
     validates :category_name_id
     validates :condition_id
     validates :delivery_burden_id
     validates :shipping_area_id
     validates :shipping_days_id
-    validates :price, format: { with: /\A[a-z0-9]+\z/i, message: 'は半角英数で入力してください。' }
   end
+ 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
